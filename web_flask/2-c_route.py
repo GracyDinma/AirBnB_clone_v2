2b7c2bbe5b405b313c4235f06c4be27bc874af7d
GracyDinma <okohagchidinma@gmail.com>
2024-07-28 16:21:58 -0700

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-"""Starts a Flask web application
-web application must be listening on 0.0.0.0, port 5000
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text
-variable (replace underscore _ symbols with a space )
 """
-
+    Sript that starts a Flask web application
+ """
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home():
-    """Return Hello HBNB"""
-    return ("Hello HBNB!")
+def hello_hbn():
+    """
+        function to return Hello HBNB!
+    """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return HBNB"""
-    return ("HBNB")
+    """
+        function to return HBNB
+    """
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """Return C followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return f"C {text}
-
-
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+def text_var(text):
+    """
+        function to display text variable passed in
+    """
+    return "C {}".format(text.replace("_", " "))
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
